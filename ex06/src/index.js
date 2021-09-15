1488b858f65ada264d73af3eba88c2ed12e40a8a
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-09-15 11:44:41 +0200

    Understanding Uninitialized Variables

---
+++ ex06/src/index.js
@@ -0,0 +1,23 @@
+function main() {
+    // Only change code below this line
+
+    var x = 5;
+
+    var y = 8;
+
+    var z = "My name is ";
+    // Only change code above this line
+
+    x = x + 2;
+
+    y = y + 4;
+
+    z = z + " John!";
+
+    return [x, y, z];
+
+}
+
+console.log(main());
+
+module.exports = main;
