9d6acfacf5a4f0349fe0f4363ab5d63a2a0aa89d
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-09-15 11:04:28 +0200

    komentari

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+
+    // komentar
+
+    /* vise linija
+    komentar  */
+
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
